3f5683e00c8f65d34c60c5a4d9a498aaf90b5606
Adam Train <adam.train@invoiced.com>
2019-11-14 11:28:32 -0600

    style consistency

--- invoicedapi/Entities/Invoice.cs
+++ invoicedapi/Entities/Invoice.cs
@@ -4,139 +4,140 @@ using Newtonsoft.Json;
 
 namespace Invoiced
 {
-public class Invoice : Entity<Invoice>
-{
 
-	public Invoice(Connection conn) : base(conn) {
+	public class Invoice : Entity<Invoice>
+	{
+
+		public Invoice(Connection conn) : base(conn) {
+		}
+
+		public Invoice() : base(){
+
+		}
+
+		public bool ShouldSerializeId() {
+			return false;
+		}
+
+		override public long EntityID() {
+			return this.Id;
+		}
+
+		override public string EntityName() {
+			return "invoices";
+		}
+
+		override public bool HasCRUD() {
+			return true;
+		}
+
+		override public bool HasList() {
+			return true;
+		}
+
+
+		[JsonProperty("id")]
+		public long Id { get;set; }
+
+		[JsonProperty("object")]
+		public string Object { get; set; }
+
+		[JsonProperty("customer")]
+		public long Customer { get; set; }
+
+		[JsonProperty("name")]
+		public object Name { get; set; }
+
+		[JsonProperty("currency")]
+		public string Currency { get; set; }
+
+		[JsonProperty("draft")]
+		public bool Draft { get; set; }
+
+		[JsonProperty("closed")]
+		public bool Closed { get; set; }
+
+		[JsonProperty("paid")]
+		public bool Paid { get; set; }
+
+		[JsonProperty("status")]
+		public string Status { get; set; }
+
+		[JsonProperty("chase")]
+		public bool Chase { get; set; }
+
+		[JsonProperty("next_chase_on")]
+		public object NextChaseOn { get; set; }
+
+		[JsonProperty("autopay")]
+		public bool Autopay { get; set; }
+
+		[JsonProperty("attempt_count")]
+		public int AttemptCount { get; set; }
+
+		[JsonProperty("next_payment_attempt")]
+		public object NextPaymentAttempt { get; set; }
+
+		[JsonProperty("subscription")]
+		public object Subscription { get; set; }
+
+		[JsonProperty("number")]
+		public string Number { get; set; }
+
+		[JsonProperty("date")]
+		public long Date { get; set; }
+
+		[JsonProperty("due_date")]
+		public long? DueDate { get; set; }
+
+		[JsonProperty("payment_terms")]
+		public string PaymentTerms { get; set; }
+
+		[JsonProperty("items")]
+		public IList<LineItem> Items { get; set; }
+
+		[JsonProperty("notes")]
+		public object Notes { get; set; }
+
+		[JsonProperty("subtotal")]
+		public double Subtotal { get; set; }
+
+		[JsonProperty("discounts")]
+		public IList<object> Discounts { get; set; }
+
+		[JsonProperty("taxes")]
+		public IList<Tax> Taxes { get; set; }
+
+		[JsonProperty("total")]
+		public double Total { get; set; }
+
+		[JsonProperty("balance")]
+		public double Balance { get; set; }
+
+		[JsonProperty("url")]
+		public string Url { get; set; }
+
+		[JsonProperty("payment_url")]
+		public string PaymentUrl { get; set; }
+
+		[JsonProperty("pdf_url")]
+		public string PdfUrl { get; set; }
+
+		[JsonProperty("created_at")]
+		public long CreatedAt { get; set; }
+
+		[JsonProperty("metadata")]
+		public  Metadata Metadata { get; set; }
+
+		[JsonProperty("ship_to")]
+		public  Metadata ShipTo { get; set; }
+
+		[JsonProperty("attachments")]
+		public IList<long> Attachments { get; set; }
+
+		[JsonProperty("disabled_payment_methods")]
+		public  IList<string> DisabledPaymentMethods { get; set; }
+
 	}
-
-	public Invoice() : base(){
-
-	}
-
-	public bool ShouldSerializeId() {
-        return false;
-    }
-
-	override public long EntityID() {
-		return this.Id;
-	}
-
-	override public string EntityName() {
-		return "invoices";
-	}
-
-	override public bool HasCRUD() {
-		return true;
-	}
-
-	override public bool HasList() {
-		return true;
-	}
-
-
-	[JsonProperty("id")]
-	public long Id { get;set; }
-
-	[JsonProperty("object")]
-	public string Object { get; set; }
-
-	[JsonProperty("customer")]
-	public long Customer { get; set; }
-
-	[JsonProperty("name")]
-	public object Name { get; set; }
-
-	[JsonProperty("currency")]
-	public string Currency { get; set; }
-
-	[JsonProperty("draft")]
-	public bool Draft { get; set; }
-
-	[JsonProperty("closed")]
-	public bool Closed { get; set; }
-
-	[JsonProperty("paid")]
-	public bool Paid { get; set; }
-
-	[JsonProperty("status")]
-	public string Status { get; set; }
-
-	[JsonProperty("chase")]
-	public bool Chase { get; set; }
-
-	[JsonProperty("next_chase_on")]
-	public object NextChaseOn { get; set; }
-
-	[JsonProperty("autopay")]
-	public bool Autopay { get; set; }
-
-	[JsonProperty("attempt_count")]
-	public int AttemptCount { get; set; }
-
-	[JsonProperty("next_payment_attempt")]
-	public object NextPaymentAttempt { get; set; }
-
-	[JsonProperty("subscription")]
-	public object Subscription { get; set; }
-
-	[JsonProperty("number")]
-	public string Number { get; set; }
-
-	[JsonProperty("date")]
-	public long Date { get; set; }
-
-	[JsonProperty("due_date")]
-	public long? DueDate { get; set; }
-
-	[JsonProperty("payment_terms")]
-	public string PaymentTerms { get; set; }
-
-	[JsonProperty("items")]
-	public IList<LineItem> Items { get; set; }
-
-	[JsonProperty("notes")]
-	public object Notes { get; set; }
-
-	[JsonProperty("subtotal")]
-	public double Subtotal { get; set; }
-
-	[JsonProperty("discounts")]
-	public IList<object> Discounts { get; set; }
-
-	[JsonProperty("taxes")]
-	public IList<Tax> Taxes { get; set; }
-
-	[JsonProperty("total")]
-	public double Total { get; set; }
-
-	[JsonProperty("balance")]
-	public double Balance { get; set; }
-
-	[JsonProperty("url")]
-	public string Url { get; set; }
-
-	[JsonProperty("payment_url")]
-	public string PaymentUrl { get; set; }
-
-	[JsonProperty("pdf_url")]
-	public string PdfUrl { get; set; }
-
-	[JsonProperty("created_at")]
-	public long CreatedAt { get; set; }
-
-	[JsonProperty("metadata")]
-	public  Metadata Metadata { get; set; }
-
-	[JsonProperty("ship_to")]
-	public  Metadata ShipTo { get; set; }
-
-	[JsonProperty("attachments")]
-	public IList<long> Attachments { get; set; }
-
-	[JsonProperty("disabled_payment_methods")]
-	public  IList<string> DisabledPaymentMethods { get; set; }
-
-}
 
 }
