61d13b3040caf82e4ec04b54f3ce9700d82341e5
Adam Train <adam.train@invoiced.com>
2019-11-18 10:25:19 -0600

    added TextMessage.cs

--- invoicedapi/Entities/TextMessage.cs
+++ invoicedapi/Entities/TextMessage.cs
@@ -5,10 +5,10 @@ using Newtonsoft.Json;
 namespace Invoiced
 {
 
-	public class Letter : Item
+	public class TextMessage : Item
 	{
 		
-		public Letter() : base() {
+		public TextMessage() : base() {
 
 		}
 
@@ -18,20 +18,17 @@ namespace Invoiced
 		[JsonProperty("state")]
 		public string State { get; set; }
 
-        [JsonProperty("num_pages")]
-		public long NumPages { get; set; }
-
-        [JsonProperty("expected_delivery_date")]
-		public long ExpectedDeliveryDate { get; set; }
-
         [JsonProperty("to")]
 		public string To { get; set; }
+
+        [JsonProperty("message")]
+		public string Message { get; set; }
 
         [JsonProperty("created_at")]
 		public long CreatedAt { get; set; }
 
 		override public string EntityId() {
-			return this.Id.ToString();
+			return this.Id;
 		}
 		
 	}
