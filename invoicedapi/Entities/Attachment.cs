dcc51c7478b9dbe4c001d8d9e05b4bb4a2398a6c
Adam Train <adam.train@invoiced.com>
2019-11-18 09:26:41 -0600

    Added Attachment.cs

---
+++ invoicedapi/Entities/Attachment.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Invoiced
+{
+
+	public class Attachment : Item
+	{
+		
+		public Attachment() : base() {
+
+		}
+
+		[JsonProperty("id")]
+		public long Id { get; set; }
+
+		[JsonProperty("file")]
+		public File File { get; set; }
+
+        [JsonProperty("created_at")]
+		public long CreatedAt { get; set; }
+
+
+		override public long EntityID() {
+			return this.Id;
+		}
+		
+	}
+
+}
