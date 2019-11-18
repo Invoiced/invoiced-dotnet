f8512d0f87d3872564f51171314e6fd02ae03662
Adam Train <adam.train@invoiced.com>
2019-11-18 11:12:02 -0600

    added Contact.cs

---
+++ invoicedapi/Entities/Contact.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Invoiced
+{
+
+	public class Contact : Entity<Contact>
+	{
+
+		public Contact(Connection conn) : base(conn) {
+		}
+
+		public Contact() : base(){
+
+		}
+
+		public override long EntityId() {
+			return this.Id;
+		}
+
+		public override string EntityIdString() {
+			return this.Id.ToString();
+		}
+
+		public override string EntityName() {
+			return "contacts";
+		}
+
+		public override bool HasCRUD() {
+			return true;
+		}
+
+		public override bool HasList() {
+			return true;
+		}
+
+		public override bool HasStringId() {
+			return false;
+		}
+
+		[JsonProperty("id")]
+		public long Id { get; set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("email")]
+		public string Email { get; set; }
+
+		[JsonProperty("primary")]
+		public bool Primary { get; set; }
+
+        [JsonProperty("address1")]
+		public string Address1 { get; set; }
+
+        [JsonProperty("address2")]
+		public string Address2 { get; set; }
+
+        [JsonProperty("city")]
+		public string City { get; set; }
+
+        [JsonProperty("state")]
+		public string State { get; set; }
+
+        [JsonProperty("postal_code")]
+		public string PostalCode { get; set; }
+
+        [JsonProperty("country")]
+		public string Country { get; set; }
+
+		[JsonProperty("created_at")]
+		public long CreatedAt { get; set; }
+
+		[JsonProperty("title")]
+		public string Title { get; set; }
+
+		[JsonProperty("phone")]
+		public string Phone { get; set; }
+
+		[JsonProperty("sms_enabled")]
+		public bool SmsEnabled { get; set; }
+
+		[JsonProperty("department")]
+		public string Department { get; set; }
+
+	}
+
+}
