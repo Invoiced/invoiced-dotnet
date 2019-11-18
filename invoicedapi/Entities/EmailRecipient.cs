c8e63a0319d1e99ea356f2e7b7758dcaf001c723
Adam Train <adam.train@invoiced.com>
2019-11-18 09:20:52 -0600

    add email aux objects

---
+++ invoicedapi/Entities/EmailRecipient.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Invoiced
+{
+
+	public class EmailRecipient : Item
+	{
+		
+		public EmailRecipient() : base() {
+
+		}
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("email")]
+		public string Email { get; set; }
+
+
+		override public long EntityID() {
+			throw new EntityException("Email recipients have no ID");
+		}
+		
+	}
+
+}
---
+++ invoicedapi/Entities/EmailRequest.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Invoiced
+{
+
+	public class EmailRequest : Item
+	{
+		
+		public EmailRequest() : base() {
+
+		}
+
+		[JsonProperty("to")]
+		public EmailRecipient[] to { get; set; }
+
+		[JsonProperty("bcc")]
+		public string Bcc { get; set; }
+
+        [JsonProperty("subject")]
+		public string Subject { get; set; }
+
+        [JsonProperty("message")]
+		public string Message { get; set; }
+
+        [JsonProperty("type")]
+		public string Type { get; set; }
+
+        [JsonProperty("start")]
+		public long Start { get; set; }
+
+        [JsonProperty("end")]
+		public long End { get; set; }
+
+        [JsonProperty("items")]
+		public string Items { get; set; }
+
+		override public long EntityID() {
+			throw new EntityException("Email requests have no ID");
+		}
+		
+	}
+
+}
