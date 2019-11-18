572d7281ca67f3986ed4c092f99afaae12f930f2
Adam Train <adam.train@invoiced.com>
2019-11-18 10:36:11 -0600

    added TextRequest.cs

---
+++ invoicedapi/Entities/TextRequest.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Invoiced
+{
+
+	public class TextRequest : Item
+	{
+		
+		public TextRequest() : base() {
+
+		}
+
+		[JsonProperty("to")]
+		public TextRecipient[] To { get; set; }
+
+		[JsonProperty("message")]
+		public string Message { get; set; }
+
+        [JsonProperty("type")]
+		public long Type { get; set; }
+
+        [JsonProperty("start")]
+		public long Start { get; set; }
+
+        [JsonProperty("end")]
+		public long end { get; set; }
+
+        [JsonProperty("items")]
+		public string Items { get; set; }
+
+		public override string EntityId() {
+			return "TextRequest";
+            // this is only used for json heading in ToString()
+		}
+		
+	}
+
+}
