58e885349df1b51a6a8f647ffab03453347f6836
Adam Train <adam.train@invoiced.com>
2019-11-18 11:25:10 -0600

    added RefundRequest

---
+++ invoicedapi/Entities/RefundRequest.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Invoiced
+{
+
+	public class RefundRequest : Item
+	{
+		
+		public RefundRequest(long amount) : base() {
+            this.Amount = amount;
+		}
+
+		[JsonProperty("amount")]
+		public long Amount { get; set; }
+
+		public override string EntityId() {
+			return "RefundRequest";
+            // this is only used for json heading in ToString()
+		}
+		
+	}
+
+}
