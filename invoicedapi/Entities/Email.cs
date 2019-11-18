3e94df8d90ddf4262e0a2963afaa3021d7ab36e1
Adam Train <adam.train@invoiced.com>
2019-11-18 09:12:40 -0600

    added Email.cs

---
+++ invoicedapi/Entities/Email.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Invoiced
+{
+
+	public class Email : Item
+	{
+		
+		public Email() : base() {
+
+		}
+
+		[JsonProperty("id")]
+		public long Id { get; set; }
+
+		[JsonProperty("state")]
+		public string State { get; set; }
+
+		[JsonProperty("reject_reason")]
+		public string RejectReason { get; set; }
+
+		[JsonProperty("email")]
+		public string EMail { get; set; } // must have diff name than enclosing type; accomplished through archaic capitalization of email
+
+        [JsonProperty("template")]
+		public string template { get; set; }
+
+        [JsonProperty("subject")]
+		public string subject { get; set; }
+
+        [JsonProperty("message")]
+		public string Message { get; set; }
+
+        [JsonProperty("opens")]
+		public int opens { get; set; }
+
+        [JsonProperty("opens_detail")]
+		public object OpensDetail { get; set; }
+
+        [JsonProperty("created_at")]
+		public long CreatedAt { get; set; }
+
+		override public long EntityID() {
+			return this.Id;
+		}
+		
+	}
+
+}
